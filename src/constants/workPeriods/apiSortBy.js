619afe5fe83ea11588768ffd092256312c3f9628
Maksym Mykhailenko <maxceem@gmail.com>
2021-05-31 14:15:39 +0300

    fix: user RB.memberRate to show weekly rate

--- src/constants/workPeriods/apiSortBy.js
+++ src/constants/workPeriods/apiSortBy.js
@@ -1,6 +1,6 @@
 export const USER_HANDLE = "workPeriods.userHandle";
 export const START_DATE = "startDate";
 export const END_DATE = "endDate";
-export const WEEKLY_RATE = "workPeriods.customerRate";
+export const WEEKLY_RATE = "memberRate";
 export const PAYMENT_STATUS = "workPeriods.paymentStatus";
 export const WORKING_DAYS = "workPeriods.daysWorked";
